584989bfcfc06af79967712e1445c29ce5afc8d2
Parth Patel <parth.patel269813@gmail.com>
2021-09-20 13:02:32 +0530

    Add project files.

---
+++ PizzaShopOnlineBAU.Site/Controllers/DeliveryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaShopOnlineBAU.Site.Controllers
+{
+    public class DeliveryController : Controller
+    {
+        public IActionResult Customer()
+        {
+            return View();
+        }
+
+        public ActionResult Customer()
+        {
+            return View();
+        }
+    }
+}
